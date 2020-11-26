92849862b34f4f737ad0a6d7422307c4439fd47c
Masum Rahman <1505014.mr@ugrad.cse.buet.ac.bd>
2020-11-26 17:19:16 +0600

    makemigrations for supply order solved

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 docker-compose  up  --build -d
 
 docker-compose run web python manage.py makemigrations
+docker-compose run web python manage.py makemigrations supply_order
 docker-compose run web python manage.py migrate
 docker-compose run web python seeder.py
 
