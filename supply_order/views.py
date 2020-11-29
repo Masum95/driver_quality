7b9e68ebb5956374313ccb3375eb7ff62ada4194
Masum Rahman <1505014.mr@ugrad.cse.buet.ac.bd>
2020-11-29 11:34:37 +0600

    file reformatted

--- supply_order/views.py
+++ supply_order/views.py
@@ -9,34 +9,9 @@ from supply_order import serializers
 from supply_order import pagination
 from supply_order import parameters
 from supply_order import utils
+
 count_response = openapi.Response('response description', serializers.CountOrderSerializer)
 get_list_response = openapi.Response('response description', serializers.SupplyOrderSerializer)
-
-
-class SupplyOrderListCreateAPIView(generics.ListCreateAPIView):
-    """
-        **List/Create SupplyOrder**
-    """
-    serializer_class = serializers.SupplyOrderSerializer
-    pagination_class = pagination.CustomPagination
-    queryset = models.SupplyOrder.objects.all()
-
-    def filter_queryset(self, queryset):
-        query_params = self.request.query_params
-        if 'supply_id' in query_params:
-            queryset = queryset.get_orders_against_supply_id(supply_id=query_params.get('supply_id'))
-        if 'how_many_last' in query_params:
-            queryset = queryset.get_last_n_records(n=query_params.get('how_many_last'))
-        return queryset
-
-    @swagger_auto_schema(manual_parameters=[parameters.SUPPLY_ID_QUERY_PARAMETER,
-                                            parameters.HOW_MANY_LAST_QUERY_PARAMETER],
-                         responses={200: get_list_response})
-    def get(self, request, *args, **kwargs):
-        return super(SupplyOrderListCreateAPIView, self).get(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return super(SupplyOrderListCreateAPIView, self).post(request, *args, **kwargs)
 
 
 @swagger_auto_schema(method='get', manual_parameters=[parameters.SUPPLY_ID_QUERY_PARAMETER,
@@ -79,3 +54,35 @@ def supply_order_count(request):
 
     return JsonResponse({'percent': percent,
                          'message': utils.get_message(percent=percent)}, safe=False)
+
+
+class SupplyOrderListCreateAPIView(generics.ListCreateAPIView):
+    """
+        **List/Create SupplyOrder**
+    """
+    serializer_class = serializers.SupplyOrderSerializer
+    pagination_class = pagination.CustomPagination
+    queryset = models.SupplyOrder.objects.all()
+
+    def filter_queryset(self, queryset):
+        query_params = self.request.query_params
+        how_many = str(100)
+
+        if 'supply_id' in query_params:
+            queryset = queryset.get_orders_against_supply_id(supply_id=query_params.get('supply_id'))
+
+        if 'how_many_last' in query_params:
+            how_many = query_params.get('how_many_last')
+
+        queryset = queryset.get_last_n_records(n=how_many)
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=[parameters.SUPPLY_ID_QUERY_PARAMETER,
+                                            parameters.HOW_MANY_LAST_QUERY_PARAMETER],
+                         responses={200: get_list_response})
+    def get(self, request, *args, **kwargs):
+        return super(SupplyOrderListCreateAPIView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super(SupplyOrderListCreateAPIView, self).post(request, *args, **kwargs)
+
